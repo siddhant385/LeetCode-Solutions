c0ae5b748796057844944f6d7554f232b74ee7c5
Siddhant Sharma <79889236+siddhant385@users.noreply.github.com>
2026-07-18 22:34:18 +0530

    Add solution for Range Sum Query - Immutable (Easy)

---
+++ 0303-range-sum-query-immutable/solution.py
@@ -0,0 +1,23 @@
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        self.prefix = self.prefixSum(nums)
+    
+    def prefixSum(self,nums):
+        n = len(nums)
+        prefix = [0 for i in range(n+1)]
+        for i in range(len(nums)):
+            prefix[i+1] = prefix[i] + nums[i]
+        return prefix
+
+        
+
+    def sumRange(self, left: int, right: int) -> int:
+        
+        return self.prefix[right+1]-self.prefix[left]
+        
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
